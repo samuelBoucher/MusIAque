5e33ee93f4533ffbbe2a811b0a8c41d8d2e464ea
Unknown <samuel.boucher@outlook.com>
2017-11-01 18:00:14 -0400

    Enlever les commentaires

--- mml.py
+++ mml.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 class MllFormat:
@@ -9,7 +10,19 @@ class MllFormat:
         for filename in os.listdir(self.path):
             mml_content_list.append(open(self.path + filename).read())
 
-        print(mml_content_list)
+        self.remove_comments(mml_content_list)
+
+    # La conversion de MIDI en MML utilisée est le logiciel 3MLE.
+    # Pendant la conversion, 3MLE ajoute des commentaires pour mieux organiser le fichier.
+    # Nous n'en avons pas besoin: nous avons seulement besoin des "notes de musique".
+    def remove_comments(self, mml_content_list):
+        for content in mml_content_list:
+            content = re.sub(re.compile("/\*.*?\*/"), "",
+                             content)  # Enlever tous les commentaires /* ... */
+            content = re.sub(re.compile("//.*?\n"), "",
+                             content)  # Enlever tous les commentaires //
+            print(content)
+            return content
 
 
 if __name__ == '__main__':
