0ab762547dcbec682e44f82a2f8655efbe92555e
Unknown <samuel.boucher@outlook.com>
2017-11-17 10:39:31 -0500

    settings_track

--- character_to_csv.py
+++ character_to_csv.py
@@ -16,18 +16,29 @@ class CharacterToCsv:
     def get_csv_content(self, content):
         rows = []
         nb_tracks = self.get_nb_tracks(content)
-        rows.append(self.generate_header(nb_tracks))
+        header = self.generate_header(nb_tracks)
+        rows.append(header)
+        settings_tracks = self.generate_settings_track()
+        for setting_row in settings_tracks:
+            rows.append(setting_row)
 
     def get_nb_tracks(self, content):
         content_without_repetition = content.replace('~', '')
         chords = content_without_repetition.split(" ")
         nb_tracks = len(max(chords, key=len))
-        print(nb_tracks)
         return nb_tracks
 
     def generate_header(self, nb_tracks):
-        pass
-        # 0, 0, Header, 1, [nb tracks], 96
+        return ['0,', '0,', 'Header,', '1,', str(nb_tracks) + ',', '96']
+
+    def generate_settings_track(self):
+        return [
+            ['1,', '0,', 'Start_track'],
+            ['1,', '0,', 'Time_signature,', '4,', '2,', '24,', '8'],
+            ['1,', '0,', 'Tempo,', '500000'],
+            ['1,', '0,', 'Program_c,', '1,', '6'],
+            ['1,', '0,', 'End_track']
+        ]
 
 
 if __name__ == '__main__':
