b10523af4d7fc6b05da8e6ea6165eb1304994086
Unknown <samuel.boucher@outlook.com>
2017-11-17 11:49:51 -0500

    separation des tracks (a part notes repetees)

--- character_to_csv.py
+++ character_to_csv.py
@@ -5,7 +5,6 @@ class CharacterToCsv:
     time_clock_interval = 48
 
     def __init__(self, text_file):
-        content = ""
         with open(text_file) as text:
             content = text.read()
 
@@ -16,8 +15,15 @@ class CharacterToCsv:
     def get_csv_content(self, content):
         rows = []
         nb_tracks = self.get_nb_tracks(content)
+        self.append_header(nb_tracks, rows)
+        self.append_settings_track(rows)
+        self.append_notes_tracks(content, nb_tracks, rows)
+
+    def append_header(self, nb_tracks, rows):
         header = self.generate_header(nb_tracks)
         rows.append(header)
+
+    def append_settings_track(self, rows):
         settings_tracks = self.generate_settings_track()
         for setting_row in settings_tracks:
             rows.append(setting_row)
@@ -39,6 +45,42 @@ class CharacterToCsv:
             ['1,', '0,', 'Program_c,', '1,', '6'],
             ['1,', '0,', 'End_track']
         ]
+
+    def append_notes_tracks(self, content, nb_tracks, rows):
+        tracks = self.get_character_tracks(content, nb_tracks)
+
+    def get_character_tracks(self, content, nb_tracks):
+        print(content)
+        tracks = []
+        for i in range(nb_tracks):
+            tracks.append([])
+        channel_counter = 0
+        repeated_note = False
+        time_clocks = 1
+        for note in content:
+            if note == '~':
+                repeated_note = True
+            elif note == ' ':
+                # 0 -> 123
+                # 1 -> 23
+                # 2 -> 3
+                for track in tracks:
+                    if len(track) < time_clocks:
+                        track.append(" ")
+                channel_counter = 0
+                time_clocks += 1
+            else:
+                if repeated_note:
+                    tracks[channel_counter].append('~' + note)
+                    repeated_note = False
+                else:
+                    tracks[channel_counter].append(note)
+
+                channel_counter += 1
+        for track in tracks:
+            print(len(track))
+            print(track)
+        return tracks
 
 
 if __name__ == '__main__':
